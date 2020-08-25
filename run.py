b94cab47b8a6eff1c4bc538da8cb5c4bf2725337
Alexander Mirvis <45396441+LynxGeekNYC@users.noreply.github.com>
2020-08-25 10:13:24 -0400

    Update run.py

--- run.py
+++ run.py
@@ -1,4 +1,3 @@
-
 import requests
 import os
 import random
@@ -6,21 +5,36 @@ import string
 import json
 
 chars = string.ascii_letters + string.digits + '!@#$%^&*()'
+
+# This is an array of email extensions; you can add whatever in here
+# todo: put this in a json file instead
+emails = ["outlook.com", "gmail.com", "yahoo.com", "hotmail.com", "aol.com", "icloud.com", "mail.com"]
+
 random.seed = (os.urandom(1024))
 
-url = 'http://craigslist.pottsfam.com/index872dijasydu2iuad27aysdu2yytaus6d2ajsdhasdasd2.php'
+# This is the URL you want to flood. Should be a login page
+url = 'https://kcs.cx/test5'
 
 names = json.loads(open('names.json').read())
+
+random.shuffle(names)
+
+n = 0
 
 for name in names:
 	name_extra = ''.join(random.choice(string.digits))
 
-	username = name.lower() + name_extra + '@yahoo.com'
+	email = name.lower() + name_extra + '@' + random.choice(emails)
+	username = name.lower() + name_extra
 	password = ''.join(random.choice(chars) for i in range(8))
+	uore = [email, username]
+	user = random.choice(uore)
 
-	requests.post(url, allow_redirects=False, data={
-		'auid2yjauysd2uasdasdasd': username,
-		'kjauysd6sAJSDhyui2yasd': password
+	r = requests.post(url, allow_redirects=False, data={
+		'username': user,
+		'password': password,
 	})
 
-	print 'sending username %s and password %s' % (username, password)
+	n += 1
+        
+	print ("%s. Sending username %s and password %s. Response: %s. Response time: %s" % (n, user, password, r.status_code, r.elapsed.total_seconds()))
